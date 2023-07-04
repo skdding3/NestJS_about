5c60c5946b56361ab75a13d270c5d8345acb9e64
skdding3 <skdding3@gmail.com>
2023-07-04 10:41:47 +0900

    nest res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BookModule],
+  imports: [BookModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
